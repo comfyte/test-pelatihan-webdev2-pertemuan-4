fdc5448736b766cb13ca6e15e6f60b8c9a786f14
Farrel R <rafirizqy12@gmail.com>
2021-03-16 14:07:04 +0700

    Create gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+// gulpfile.js
+// File konfigurasi Gulp (build tool)
+
+// Impor modul yang diperlukan
+const gulp     = require("gulp");
+const prettier = require("gulp-prettier);
+
+// Kita assign ke variabel baru agar kode lebih terbaca
+const src  = gulp.src;
+const dest = gulp.dest;
+
+// Definisikan sebuah task untuk memformat file-file kode
+function percantikKode() {
+    return src(".")
+           .pipe(prettier())
+           .pipe(dest("."));
+}
+
+// Buat agar task tersebut bisa dijalankan dari luar
+exports.default = percantikKode;
+
